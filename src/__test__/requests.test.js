0633175b4c6d124fcba297629b9c6c66ddc313e1
marcmacias96 <mamacias3@espe.edu.ec>
2019-12-03 20:28:30 -0500

    test add push request

--- src/__test__/requests.test.js
+++ src/__test__/requests.test.js
@@ -1,8 +1,25 @@
 import request from "supertest";
 import app from "../app/app";
-import mongoose from "mongoose";
-var server;
-describe("Test de funcionamiento de API-REST", () => {
+
+describe("Test de funcionamiento de peticiones Get API-REST", () => {
+  test("Creando un nuevo usuario", async () => {
+    const user = {
+      usr_names: "Jose",
+      usr_lastNames: "Perez",
+      usr_user: "josep",
+      usr_email: "jose@gmail.com",
+      usr_password: "12345",
+      usr_personalID: 1315204105
+    };
+    const response = await request(app)
+      .post("/users")
+      .send(user)
+      .set("Accept", "application/json");
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+describe("Test de funcionamiento de peticiones Get API-REST", () => {
   test("Obteniendo todos los usuarios del API", async () => {
     const response = await request(app).get("/users");
     expect(response.statusCode).toBe(200);
